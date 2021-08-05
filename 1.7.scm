4d07ade4d42c7902b4bfc1a36955c87697214b6f
rn7s2 <a3788670@foxmail.com>
2021-08-05 12:13:48 +0800

    完善习题

---
+++ 1.7.scm
@@ -0,0 +1,19 @@
+;; 假如x是0.0000000001，正确的平方根是0.000001，但是由于
+;; 选定的猜测值比较小，第一次good-enough?就判断为真了。
+
+;; 假如x是很大的数，而计算机的精度有限，很可能平方的精度低于
+;; good-enough?选定的0.001，就永远得不出结果。
+
+;; 新定义的求平方根过程
+(define (sqrt x)
+  (define (sqrt-iter guess old-guess x)
+    (define (good-enough? guess old-guess x)
+      (< (/ (abs (- guess old-guess)) guess) 0.001))
+    (define (improve guess x)
+      (define (average x y)
+        (/ (+ x y) 2.0))
+      (average guess (/ x guess)))
+    (if (good-enough? guess old-guess x)
+        guess
+        (sqrt-iter (improve guess x) guess x)))
+  (sqrt-iter 1.0 0 x))
